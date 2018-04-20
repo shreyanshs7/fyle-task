98aead2b74cadd265addb742a83dadb23d276cc8
shreyanshs7 <shreyanshss7@gmail.com>
2018-04-20 23:00:55 +0530

    Changed settings

--- fyle/settings.py
+++ fyle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6dpi)eoj@ex!^m7=ro#d&@)p5q%@uriu3kc&lp7qry@&jen#36'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fyle-task.herokuapp.com']
 
 
 # Application definition
